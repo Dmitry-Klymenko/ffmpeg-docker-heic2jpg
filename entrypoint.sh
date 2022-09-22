660b7dd3ec35fa65804f7bc343dbeb10d6bd219d
Dmitry Klymenko <dmitry.klymenko@gmail.com>
2022-09-22 18:39:12 +1000

    Skipping already converted files

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,16 @@ cd /input
 
 find "./$path" -type f | egrep -i "$mask" | sort | cut -c 3- | while read input
 do
-
 	output="/output/${input%.*}.$ext"
-	mkdir -p "$(dirname "$output")"
-	nice -n $nice ffmpeg -n -hide_banner -v info -i "$input" "$@" "$output" < /dev/null || true
-	nice -n $nice exiftool -overwrite_original -TagsFromFile "$input" "-all:all>all:all" "$output" < /dev/null || true
-	nice -n $nice touch --reference="$input" "$output" < /dev/null || true
+
+	if [ ! -f $output ]
+	then
+		mkdir -p "$(dirname "$output")"
+		nice -n $nice ffmpeg -n -hide_banner -v info -i "$input" "$@" "$output" < /dev/null || true
+		nice -n $nice exiftool -overwrite_original -TagsFromFile "$input" "-all:all>all:all" "$output" < /dev/null || true
+		nice -n $nice touch --reference="$input" "$output" < /dev/null || true
+	else
+		echo "File already exists, skipping: $output"
+	fi
 
 done
